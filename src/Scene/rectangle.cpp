161a43a5eeb9567c324cfc455ae0abbe6c99aeb9
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2020-01-05 17:48:11 +1000

    add corner colors to scene::rectangle

--- src/Scene/rectangle.cpp
+++ src/Scene/rectangle.cpp
@@ -5,11 +5,18 @@ using namespace Scene;
 void Rectangle::draw()
 {
 	auto model = glm::scale(getTransform(), { getSize(), 1.0f });
+	auto color = getColor();
 
-	if (mFilled)
-		GRAPHICS->drawRectangle(model, getColor());
-	else
-		GRAPHICS->drawLineRectangle(model, getColor());
-	
+	std::vector<Renderer::Vertex::PositionColor> vertices = {
+		{ { 0.0f, 0.0f, 0.0f }, { mCornerColors.at(Corner::TopLeft) * color } },
+		{ { 0.0f, 1.0f, 0.0f }, { mCornerColors.at(Corner::BottomLeft) * color } },
+		{ { 1.0f, 1.0f, 0.0f }, { mCornerColors.at(Corner::BottomRight) * color } },
+		{ { 1.0f, 0.0f, 0.0f }, { mCornerColors.at(Corner::TopRight) * color } }
+	};
+
+	static const std::vector<uint32_t> indices = { 0, 1, 2, 0, 2, 3 };
+
+	GRAPHICS->draw(Renderer::Topology::TriangleList, vertices, indices, model);
+
 	Node::draw();
 }
--- src/Scene/rectangle.h
+++ src/Scene/rectangle.h
@@ -7,14 +7,28 @@ namespace Scene
 {
 	class Rectangle : public Node, public Color
 	{
+	public:
+		enum class Corner
+		{
+			TopLeft,
+			TopRight,
+			BottomLeft,
+			BottomRight
+		};
+
 	protected:
 		void draw() override;
 
 	public:
-		bool isFilled() const { return mFilled; }
-		void setFilled(bool value) { mFilled = value; }
+		auto getCornerColor(Corner corner) const { return mCornerColors.at(corner); }
+		void setCornerColor(Corner corner, const glm::vec4& value) { mCornerColors[corner] = value; }
 
-	private:
-		bool mFilled = true;
+	public:
+		std::map<Corner, glm::vec4> mCornerColors = {
+			{ Corner::TopLeft, { Graphics::Color::White, 1.0f } },
+			{ Corner::TopRight, { Graphics::Color::White, 1.0f } },
+			{ Corner::BottomLeft, { Graphics::Color::White, 1.0f } },
+			{ Corner::BottomRight, { Graphics::Color::White, 1.0f } },
+		};
 	};
 }
