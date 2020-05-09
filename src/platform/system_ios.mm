3c9552106b52bf0b9f718f776a6216546e825b9a
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2020-05-10 00:46:46 +1000

    make entrypoint for ios

--- src/platform/system_ios.mm
+++ src/platform/system_ios.mm
@@ -4,6 +4,44 @@
 
 using namespace Platform;
 
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@end
+
+@implementation AppDelegate
+
+- (void)runMain {
+    sky_main();
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self performSelectorOnMainThread:@selector(runMain) withObject:nil waitUntilDone:NO];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+}
+
+@end
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+
 std::shared_ptr<System> System::create(const std::string& appname)
 {
     return std::make_shared<SystemIos>(appname);
