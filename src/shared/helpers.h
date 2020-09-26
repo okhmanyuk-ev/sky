1c74bad4aed354bcb7a94d1012b1567ece0e2ed6
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2020-09-26 22:36:42 +1000

    shared: add more helpers

--- src/shared/helpers.h
+++ src/shared/helpers.h
@@ -6,4 +6,21 @@ namespace Shared::Helpers
 {
 	std::string SecondsToFmtString(int seconds);
 	bool Chance(float normalized_percent);
+
+	inline const float RadialLimit = glm::pi<float>() * 2.0f;
+
+	inline float RadialToGlobal(float radius, float radial_pos)
+	{
+		return radius * radial_pos;
+	}
+
+	inline float GlobalToRadial(float radius, float global_pos)
+	{
+		return global_pos / radius;
+	}
+
+	inline float GetPerimeter(float radius)
+	{
+		return RadialToGlobal(radius, RadialLimit);
+	}
 }
