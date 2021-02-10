116624aa74d30dcc5d0f0a5f1a43fb3b1bf72b0f
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2021-02-10 17:10:36 +1000

    networking: wip

--- src/shared/networking_ws.cpp
+++ src/shared/networking_ws.cpp
@@ -1,18 +1,52 @@
 #include "networking_ws.h"
 #include <console/device.h>
+#include <common/buffer_helpers.h>
 
 using namespace Shared::NetworkingWS;
 
 // channel
 
-void Channel::sendEvent(const std::string& name, const std::map<std::string, std::string>& params)
+void Channel::read(Common::BitBuffer& buf)
 {
-	send(name);
+	auto name = Common::BufferHelpers::ReadString(buf);
+	auto params = std::map<std::string, std::string>();
+	while (buf.readBit())
+	{
+		auto key = Common::BufferHelpers::ReadString(buf);
+		auto value = Common::BufferHelpers::ReadString(buf);
+		params.insert({ key, value });
+	}
+	//if (mShowEventLogs || mEvents.count(name) == 0)
+	{
+		LOG("event: \"" + name + "\"");
+		for (const auto& [key, value] : params)
+		{
+			LOGF(" - {} : {}", key, value);
+		}
+	}
+	if (mEvents.count(name) > 0)
+	{
+		mEvents.at(name)(params);
+	}
 }
 
-void Channel::send(const std::string& text)
+void Channel::sendEvent(const std::string& name, const std::map<std::string, std::string>& params)
 {
-	mSendCallback(text);
+	auto buf = Common::BitBuffer();
+	Common::BufferHelpers::WriteString(buf, name);
+	for (auto& [key, value] : params)
+	{
+		buf.writeBit(true);
+		Common::BufferHelpers::WriteString(buf, key);
+		Common::BufferHelpers::WriteString(buf, value);
+	}
+	buf.writeBit(false);
+	send(buf);
+}
+
+void Channel::send(const Common::BitBuffer& buf)
+{
+	mSendCallback(buf);
 }
 
 void Channel::addEventCallback(const std::string& name, EventCallback callback)
@@ -30,14 +64,25 @@ Server::Server(uint16_t port)
 
 	mWSServer.set_open_handler([this](websocketpp::connection_hdl hdl) {
 		auto channel = createChannel();
-		channel->setSendCallback([this, hdl](const auto& text) {
-			mWSServer.send(hdl, text, websocketpp::frame::opcode::value::TEXT);
+		channel->setSendCallback([this, hdl](const auto& buf) {
+			mWSServer.send(hdl, buf.getMemory(), buf.getSize(), websocketpp::frame::opcode::BINARY);
 		});
 		mChannels.insert({ hdl, channel });
 	});
 
 	mWSServer.set_close_handler([this](websocketpp::connection_hdl hdl) {
 		mChannels.erase(hdl);
+	});
+
+	mWSServer.set_message_handler([this](websocketpp::connection_hdl hdl, websocketpp::config::asio_client::message_type::ptr msg) {
+		if (mChannels.count(hdl) == 0)
+			return;
+
+		auto& payload = msg->get_raw_payload();
+		auto buf = Common::BitBuffer();
+		buf.write(payload.data(), payload.size());
+		buf.toStart();
+		mChannels.at(hdl)->read(buf);
 	});
 
 	mWSServer.init_asio();
@@ -56,13 +101,27 @@ Client::Client(const std::string& url)
 {
 	mWSClient.set_access_channels(websocketpp::log::alevel::all);
 	mWSClient.clear_access_channels(websocketpp::log::alevel::frame_payload);
-
 	mWSClient.init_asio();
 
-	using message_ptr = websocketpp::config::asio_client::message_type::ptr;
+	mWSClient.set_open_handler([this](websocketpp::connection_hdl hdl) {
+		auto channel = createChannel();
+		channel->setSendCallback([this, hdl](const auto& buf) {
+			mWSClient.send(hdl, buf.getMemory(), buf.getSize(), websocketpp::frame::opcode::BINARY);
+		});
+		mHdl = hdl;
+		mChannel = channel;
+	});
+	mWSClient.set_close_handler([this](websocketpp::connection_hdl hdl) {
+		mHdl.reset();
+		mChannel = nullptr;
+	});
 
-	mWSClient.set_message_handler([this](websocketpp::connection_hdl hdl, message_ptr msg){
-		//
+	mWSClient.set_message_handler([this](websocketpp::connection_hdl hdl, websocketpp::config::asio_client::message_type::ptr msg) {
+		auto& payload = msg->get_raw_payload();
+		auto buf = Common::BitBuffer();
+		buf.write(payload.data(), payload.size());
+		buf.toStart();
+		mChannel->read(buf);
 	});
 
 	websocketpp::lib::error_code ec;
--- src/shared/networking_ws.h
+++ src/shared/networking_ws.h
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <common/frame_system.h>
+#include <common/bitbuffer.h>
 #include <websocketpp/server.hpp>
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/config/asio_no_tls_client.hpp>
@@ -12,15 +13,18 @@ namespace Shared::NetworkingWS
 	class Channel
 	{
 	public:
-		using SendCallback = std::function<void(const std::string& text)>;
+		using SendCallback = std::function<void(const Common::BitBuffer&)>;
 		using EventCallback = std::function<void(std::map<std::string, std::string>)>;
 
 	public:
 		virtual ~Channel() {};
 
 	public:
+		void read(Common::BitBuffer& buf);
+
+	public:
 		void sendEvent(const std::string& name, const std::map<std::string, std::string>& params = {});
-		void send(const std::string& text);
+		void send(const Common::BitBuffer& buf);
 
 	public:
 		void addEventCallback(const std::string& name, EventCallback callback);
@@ -66,9 +70,18 @@ namespace Shared::NetworkingWS
 		Client(const std::string& url);
 
 	public:
+		virtual std::shared_ptr<Channel> createChannel() = 0;
+
+	public:
 		void frame() override;
+
+	public:
+		auto getChannel() const { return mChannel; }
+		bool isConnected() const { return mChannel != nullptr; }
 
 	private:
 		WSClient mWSClient;
+		websocketpp::connection_hdl mHdl;
+		std::shared_ptr<Channel> mChannel = nullptr;
 	};
 }
