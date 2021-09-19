08b573853be7ba88000ce35d48b5d1cbff9fe3f6
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2021-09-19 19:17:30 +1000

    simplify code in grayscale shader

--- src/renderer/shaders/grayscale.h
+++ src/renderer/shaders/grayscale.h
@@ -1,37 +1,21 @@
 #pragma once
 
-#include <renderer/shader_custom.h>
+#include <renderer/shaders/default.h>
 
 namespace Renderer::Shaders
 {
-	class Grayscale : public ShaderCustom
+	class Grayscale : public Default
 	{
 	private:
 		struct alignas(16) CustomConstantBuffer
 		{
-			glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 			float intensity = 1.0f;
 		};
 
 	public:
-		enum class Flag
-		{
-			Textured,
-			Colored
-		};
-
-	private:
-		static std::set<Flag> MakeFlagsFromLayout(const Vertex::Layout& layout);
-		static std::string MakeDefinesFromFlags(const Vertex::Layout& layout, const std::string& source, const std::set<Flag>& flags);
-
-	public:
-		Grayscale(const Vertex::Layout& layout, const std::set<Flag>& flags);
 		Grayscale(const Vertex::Layout& layout);
 
 	public:
-		auto getColor() const { return mCustomConstantBuffer.color; }
-		void setColor(const glm::vec4& value) { mCustomConstantBuffer.color = value; }
-
 		auto getIntensity() const { return mCustomConstantBuffer.intensity; }
 		void setIntensity(float value) { mCustomConstantBuffer.intensity = value; }
 
