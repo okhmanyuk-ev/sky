aee2d0df53b49a85e6733cc1912ea8e08b2f4791
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2022-06-06 22:35:11 +1000

    remove blur2 shader, remove custom shader

--- src/renderer/shader_custom.h
+++ src/renderer/shader_custom.h
@@ -8,47 +8,6 @@
 
 namespace Renderer
 {
-	class ShaderCustom : public Shader, public ShaderMatrices
-	{
-	private:
-		struct alignas(16) ConstantBuffer
-		{
-			glm::mat4 view = glm::mat4(1.0f);
-			glm::mat4 projection = glm::mat4(1.0f);
-			glm::mat4 model = glm::mat4(1.0f);
-		};
-
-	public:
-		ShaderCustom(const Vertex::Layout& layout, const std::set<Vertex::Attribute::Type>& requiredAttribs,
-			size_t customConstantBufferSize, const std::string& source);
-		virtual ~ShaderCustom();
-
-	protected:
-		void apply() override;
-		void update() override;
-
-	public:
-		void setCustomConstantBuffer(void* value) { mCustomConstantBuffer = value; }
-
-	public:
-		glm::mat4 getProjectionMatrix() const override { return mConstantBuffer.projection; }
-		void setProjectionMatrix(const glm::mat4& value) override { mConstantBuffer.projection = value; }
-
-		glm::mat4 getViewMatrix() const override { return mConstantBuffer.view; }
-		void setViewMatrix(const glm::mat4& value) override { mConstantBuffer.view = value; }
-
-		glm::mat4 getModelMatrix() const override { return mConstantBuffer.model; }
-		void setModelMatrix(const glm::mat4& value) override { mConstantBuffer.model = value; }
-
-	private:
-		ConstantBuffer mConstantBuffer;
-		void* mCustomConstantBuffer = nullptr;
-
-	private:
-		struct Impl;
-		std::unique_ptr<Impl> mImpl;
-	};
-
 	class ShaderCross : public Shader
 	{
 	public:
--- src/renderer/shaders/blur.h
+++ src/renderer/shaders/blur.h
@@ -45,39 +45,4 @@ namespace Renderer::Shaders
 	private:
 		Settings mSettings;
 	};
-
-	class Blur2 : public ShaderCustom
-	{
-	public:
-		enum class Direction
-		{
-			Vertical,
-			Horizontal
-		};
-
-	private:
-		struct alignas(16) ConstantBuffer
-		{
-			glm::vec2 direction = { 0.0f, 0.0f };
-			glm::vec2 resolution = { 0.0f, 0.0f };
-			float sigma = 1.0f;
-            float iterations = 32;
-		};
-
-	public:
-		Blur2(const Vertex::Layout& layout);
-
-	public:
-		void setDirection(Direction value) { mConstantBuffer.direction = (value == Direction::Horizontal ? glm::vec2(1.0f, 0.0f) : glm::vec2(0.0f, 1.0f)); }
-		void setResolution(const glm::vec2& value) { mConstantBuffer.resolution = value; }
-
-		auto getSigma() const { return mConstantBuffer.sigma; }
-		void setSigma(float value) { mConstantBuffer.sigma = value; }
-
-		auto getIterations() const { return (int)mConstantBuffer.iterations; }
-		void setIterations(int value) { mConstantBuffer.iterations = (float)value; }
-
-	private:
-		ConstantBuffer mConstantBuffer;
-	};
 }
