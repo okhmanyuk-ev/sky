aa442ca294b396567d7804bd633477f7dd05958e
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2022-11-27 18:36:10 +0300

    ios: fix gesture recognizer

--- src/platform/system_ios.h
+++ src/platform/system_ios.h
@@ -10,78 +10,78 @@
 
 namespace Platform
 {
-    class SystemIos : public System
-    {
-    public:
-        SystemIos(const std::string& appname);
-        ~SystemIos();
-        
-    public:
-        void process() override;
-        void quit() override { /* nothing */ }
+	class SystemIos : public System
+	{
+	public:
+		SystemIos(const std::string& appname);
+		~SystemIos();
+		
+	public:
+		void process() override;
+		void quit() override { /* nothing */ }
 
-        bool isFinished() const override { return false; }
-        
-        int getWidth() const override { return mWidth; }
-        int getHeight() const override { return mHeight; }
-        
-        float getScale() const override { return mScale; }
-        void setScale(float value) override { /* nothing */ }
-        
-        float getSafeAreaTopMargin() const override { return mSafeAreaTopMargin; }
-        float getSafeAreaBottomMargin() const override { return mSafeAreaBottomMargin; }
-        float getSafeAreaLeftMargin() const override { return mSafeAreaLeftMargin; }
-        float getSafeAreaRightMargin() const override { return mSafeAreaRightMargin; }
-        
-        bool isKeyPressed(Input::Keyboard::Key key) const override { return false; }
-        bool isKeyPressed(Input::Mouse::Button key) const override { return false; }
-        
-        void resize(int width, int height) override { /* nothing */ }
-        void setTitle(const std::string& text) override { /* nothing */ }
-        void hideCursor() override { /* nothing */ }
-        void showCursor() override { /* nothing */ }
-        void setCursorPos(int x, int y) override { /* nothing */ }
-        
-        std::string getAppName() const override { return mAppName; }
-        
-        void showVirtualKeyboard() override;
-        void hideVirtualKeyboard() override;
-        bool isVirtualKeyboardOpened() const override;
+		bool isFinished() const override { return false; }
+		
+		int getWidth() const override { return mWidth; }
+		int getHeight() const override { return mHeight; }
+		
+		float getScale() const override { return mScale; }
+		void setScale(float value) override { /* nothing */ }
+		
+		float getSafeAreaTopMargin() const override { return mSafeAreaTopMargin; }
+		float getSafeAreaBottomMargin() const override { return mSafeAreaBottomMargin; }
+		float getSafeAreaLeftMargin() const override { return mSafeAreaLeftMargin; }
+		float getSafeAreaRightMargin() const override { return mSafeAreaRightMargin; }
+		
+		bool isKeyPressed(Input::Keyboard::Key key) const override { return false; }
+		bool isKeyPressed(Input::Mouse::Button key) const override { return false; }
+		
+		void resize(int width, int height) override { /* nothing */ }
+		void setTitle(const std::string& text) override { /* nothing */ }
+		void hideCursor() override { /* nothing */ }
+		void showCursor() override { /* nothing */ }
+		void setCursorPos(int x, int y) override { /* nothing */ }
+		
+		std::string getAppName() const override { return mAppName; }
+		
+		void showVirtualKeyboard() override;
+		void hideVirtualKeyboard() override;
+		bool isVirtualKeyboardOpened() const override;
 
-        std::string getVirtualKeyboardText() const override;
-        void setVirtualKeyboardText(const std::string& text) override;
-        
-        std::string getUUID() const override;
-        
-        void initializeBilling(const ProductsMap& products) override;
-        void purchase(const std::string& product) override;
+		std::string getVirtualKeyboardText() const override;
+		void setVirtualKeyboardText(const std::string& text) override;
+		
+		std::string getUUID() const override;
+		
+		void initializeBilling(const ProductsMap& products) override;
+		void purchase(const std::string& product) override;
 
-        void haptic(HapticType hapticType) override;
+		void haptic(HapticType hapticType) override;
 
-    public:
-        void setSize(int w, int h) { mWidth = w; mHeight = h; }
-        
-    public:
-        static inline UIWindow* Window = nullptr;
+	public:
+		void setSize(int w, int h) { mWidth = w; mHeight = h; }
+		
+	public:
+		static inline UIWindow* Window = nullptr;
 
-    private:
-        UITextField* mTextField = nullptr;
-        
-    private:
-        void refreshDimensions();
-        
-    private:
-        int mWidth = 0;
-        int mHeight = 0;
-        float mScale = 0.0f;
-        std::string mAppName;
-        
-        float mSafeAreaTopMargin = 0.0f;
-        float mSafeAreaBottomMargin = 0.0f;
-        float mSafeAreaLeftMargin = 0.0f;
-        float mSafeAreaRightMargin = 0.0f;
-        
-        ProductsMap products;
-    };
+	private:
+		UITextField* mTextField = nullptr;
+		
+	private:
+		void refreshDimensions();
+		
+	private:
+		int mWidth = 0;
+		int mHeight = 0;
+		float mScale = 0.0f;
+		std::string mAppName;
+		
+		float mSafeAreaTopMargin = 0.0f;
+		float mSafeAreaBottomMargin = 0.0f;
+		float mSafeAreaLeftMargin = 0.0f;
+		float mSafeAreaRightMargin = 0.0f;
+		
+		ProductsMap products;
+	};
 }
 #endif
--- src/platform/system_ios.mm
+++ src/platform/system_ios.mm
@@ -12,72 +12,67 @@ using namespace Platform;
 
 - (void)runMain
 {
-    sky_main();
+	sky_main();
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [self performSelectorOnMainThread:@selector(runMain) withObject:nil waitUntilDone:NO];
-    return YES;
+	[self performSelectorOnMainThread:@selector(runMain) withObject:nil waitUntilDone:NO];
+	return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    //
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    //
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    //
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    //
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    //
 }
 
 - (void)emitTouchEvent:(NSSet*)touches withType:(Input::Touch::Event::Type)type
 {
-    for (UITouch* touch in touches)
-    {
-        auto location = [touch locationInView:SystemIos::Window];
-        auto e = Input::Touch::Event();
-        e.type = type;
-        e.x = location.x * PLATFORM->getScale();
-        e.y = location.y * PLATFORM->getScale();
-        EVENT->emit(e);
-        break;
-    }
+	for (UITouch* touch in touches)
+	{
+		auto location = [touch locationInView:SystemIos::Window];
+		auto e = Input::Touch::Event();
+		e.type = type;
+		e.x = location.x * PLATFORM->getScale();
+		e.y = location.y * PLATFORM->getScale();
+		EVENT->emit(e);
+		break;
+	}
 }
 
 -(void)touchesBegan:(NSSet*)touches withEvent:(__unused ::UIEvent*)event
 {
-    [self emitTouchEvent:touches withType:Input::Touch::Event::Type::Begin];
+	[self emitTouchEvent:touches withType:Input::Touch::Event::Type::Begin];
 }
 
 - (void)touchesMoved:(NSSet*)touches withEvent:(__unused ::UIEvent*)event
 {
-    [self emitTouchEvent:touches withType:Input::Touch::Event::Type::Continue];
+	[self emitTouchEvent:touches withType:Input::Touch::Event::Type::Continue];
 }
 
 - (void)touchesCancelled:(NSSet*)touches withEvent:(__unused ::UIEvent*)event
 {
-    [self emitTouchEvent:touches withType:Input::Touch::Event::Type::End];
+	[self emitTouchEvent:touches withType:Input::Touch::Event::Type::End];
 }
 
 - (void)touchesEnded:(NSSet*)touches withEvent:(__unused ::UIEvent*)event
 {
-    [self emitTouchEvent:touches withType:Input::Touch::Event::Type::End];
+	[self emitTouchEvent:touches withType:Input::Touch::Event::Type::End];
 }
 @end
 
@@ -89,32 +84,32 @@ using namespace Platform;
 
 -(bool)prefersHomeIndicatorAutoHidden
 {
-    return NO;
+	return NO;
 }
 
 -(UIRectEdge)preferredScreenEdgesDeferringSystemGestures
 {
-    return UIRectEdgeBottom;
+	return UIRectEdgeBottom;
 }
 
 // UITextFieldDelegate
 
 -(void)textFieldDidChange:(UITextField*)textField
 {
-    EVENT->emit(System::VirtualKeyboardTextChanged({ std::string(textField.text.UTF8String) }));
+	EVENT->emit(System::VirtualKeyboardTextChanged({ std::string(textField.text.UTF8String) }));
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    EVENT->emit(System::VirtualKeyboardEnterPressed());
-    return NO;
+	EVENT->emit(System::VirtualKeyboardEnterPressed());
+	return NO;
 }
 
 // SKProductsRequestDelegate
 
 - (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
 {
-    //assert(false);
+	//assert(false);
 }
 
 @end
@@ -123,166 +118,166 @@ using namespace Platform;
 
 int main(int argc, char * argv[])
 {
-    @autoreleasepool {
-        // AppDelegate must be implemented in the project
-        return UIApplicationMain(argc, argv, nil, @"AppDelegate");
-    }
+	@autoreleasepool {
+		// AppDelegate must be implemented in the project
+		return UIApplicationMain(argc, argv, nil, @"AppDelegate");
+	}
 }
 
 // ios system
 
 std::shared_ptr<System> System::create(const std::string& appname)
 {
-    return std::make_shared<SystemIos>(appname);
+	return std::make_shared<SystemIos>(appname);
 }
 
 SystemIos::SystemIos(const std::string& appname) : mAppName(appname)
 {
-    auto screen = [UIScreen mainScreen];
-    auto bounds = [screen bounds];
+	auto screen = [UIScreen mainScreen];
+	auto bounds = [screen bounds];
 
-    Window = [[UIWindow alloc]initWithFrame:bounds];
-    [Window makeKeyAndVisible];
-    
-    for (UIGestureRecognizer* recognizer in Window.gestureRecognizers)
-    {
-        recognizer.delaysTouchesBegan = false;
-        recognizer.cancelsTouchesInView = false;
-        recognizer.enabled = false;
-    }
-    
-    auto rootViewController = [[ViewController alloc] init];
-    [Window setRootViewController: rootViewController];
-    
-    auto rootView = [[UIView alloc] initWithFrame:Window.frame];
-    [rootView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
-    [rootViewController setView:rootView];
-    
-    mTextField = [[UITextField alloc] init];
-    [mTextField setDelegate:rootViewController];
-    [mTextField setKeyboardType:UIKeyboardTypeDefault];
-    [mTextField setReturnKeyType:UIReturnKeyDone];
-    [mTextField setSmartInsertDeleteType:UITextSmartInsertDeleteTypeYes];
-    [mTextField addTarget:rootViewController action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
-    [rootView addSubview:mTextField];
-        
-    refreshDimensions(); // we need mWidth & mHeight to be initialized here, but ResizeEvent may be a mistake here
+	Window = [[UIWindow alloc]initWithFrame:bounds];
+	[Window makeKeyAndVisible];
+
+	for (UIGestureRecognizer* recognizer in Window.gestureRecognizers)
+	{
+		recognizer.delaysTouchesBegan = false;
+		recognizer.delaysTouchesEnded = false;
+		recognizer.cancelsTouchesInView = false;
+		recognizer.enabled = false;
+	}
+	
+	auto rootViewController = [[ViewController alloc] init];
+	[Window setRootViewController: rootViewController];
+	
+	auto rootView = [[UIView alloc] initWithFrame:Window.frame];
+	[rootView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+	[rootViewController setView:rootView];
+	
+	mTextField = [[UITextField alloc] init];
+	[mTextField setDelegate:rootViewController];
+	[mTextField setKeyboardType:UIKeyboardTypeDefault];
+	[mTextField setReturnKeyType:UIReturnKeyDone];
+	[mTextField setSmartInsertDeleteType:UITextSmartInsertDeleteTypeYes];
+	[mTextField addTarget:rootViewController action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+	[rootView addSubview:mTextField];
+		
+	refreshDimensions(); // we need mWidth & mHeight to be initialized here, but ResizeEvent may be a mistake here
 }
 
 SystemIos::~SystemIos()
 {
-    //
+	//
 }
 
 void SystemIos::process()
 {
-    SInt32 result;
-    do {
-        result = CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, TRUE);
-    } while (result == kCFRunLoopRunHandledSource);
-    
-    refreshDimensions();
+	SInt32 result;
+	do {
+		result = CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, TRUE);
+	} while (result == kCFRunLoopRunHandledSource);
+	
+	refreshDimensions();
 }
 
 void SystemIos::showVirtualKeyboard()
 {
-    [mTextField becomeFirstResponder];
+	[mTextField becomeFirstResponder];
 }
 
 void SystemIos::hideVirtualKeyboard()
 {
-    [mTextField resignFirstResponder];
+	[mTextField resignFirstResponder];
 }
 
 bool SystemIos::isVirtualKeyboardOpened() const
 {
-    return [mTextField isFirstResponder];
+	return [mTextField isFirstResponder];
 }
 
 std::string SystemIos::getVirtualKeyboardText() const
 {
-    return [[mTextField text] UTF8String];
+	return [[mTextField text] UTF8String];
 }
 
 void SystemIos::setVirtualKeyboardText(const std::string& text)
 {
-    [mTextField setText:[NSString stringWithUTF8String:text.c_str()]];
-    EVENT->emit(System::VirtualKeyboardTextChanged({ text }));
+	[mTextField setText:[NSString stringWithUTF8String:text.c_str()]];
+	EVENT->emit(System::VirtualKeyboardTextChanged({ text }));
 }
 
 std::string SystemIos::getUUID() const
 {
-    return UIDevice.currentDevice.identifierForVendor.UUIDString.UTF8String;
+	return UIDevice.currentDevice.identifierForVendor.UUIDString.UTF8String;
 }
 
 void SystemIos::refreshDimensions()
 {
-    auto screen = [UIScreen mainScreen];
+	auto screen = [UIScreen mainScreen];
 
-    auto prev_width = mWidth;
-    auto prev_height = mHeight;
-    
-    mWidth = static_cast<int>(screen.bounds.size.width);
-    mHeight = static_cast<int>(screen.bounds.size.height);
-    
-    mScale = screen.scale;
-    
-    mWidth *= mScale;
-    mHeight *= mScale;
+	auto prev_width = mWidth;
+	auto prev_height = mHeight;
+	
+	mWidth = static_cast<int>(screen.bounds.size.width);
+	mHeight = static_cast<int>(screen.bounds.size.height);
+	
+	mScale = screen.scale;
+	
+	mWidth *= mScale;
+	mHeight *= mScale;
 
-    auto safeArea = [Window safeAreaInsets];
-    
-    mSafeAreaTopMargin = safeArea.top * mScale;
-    mSafeAreaBottomMargin = safeArea.bottom * mScale;
-    mSafeAreaLeftMargin = safeArea.left * mScale;
-    mSafeAreaRightMargin = safeArea.right * mScale;
-    
-    if (prev_width != mWidth || prev_height != mHeight)
-    {
-        EVENT->emit(ResizeEvent({ mWidth, mHeight }));
-    }
+	auto safeArea = [Window safeAreaInsets];
+	
+	mSafeAreaTopMargin = safeArea.top * mScale;
+	mSafeAreaBottomMargin = safeArea.bottom * mScale;
+	mSafeAreaLeftMargin = safeArea.left * mScale;
+	mSafeAreaRightMargin = safeArea.right * mScale;
+	
+	if (prev_width != mWidth || prev_height != mHeight)
+	{
+		EVENT->emit(ResizeEvent({ mWidth, mHeight }));
+	}
 }
 
 void SystemIos::initializeBilling(const ProductsMap& _products)
 {
-    products = _products;
-    
-    auto viewController = (ViewController*)[Window rootViewController];
-    auto names = [[NSMutableSet alloc] init];
-    
-    for (auto [_name, callback] : products)
-    {
-        auto name = [NSString stringWithUTF8String:_name.c_str()];
-        [names addObject:name];
-    }
-    
-    auto productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:names];
-    productsRequest.delegate = viewController;
-    [productsRequest start];
+	products = _products;
+	
+	auto viewController = (ViewController*)[Window rootViewController];
+	auto names = [[NSMutableSet alloc] init];
+	
+	for (auto [_name, callback] : products)
+	{
+		auto name = [NSString stringWithUTF8String:_name.c_str()];
+		[names addObject:name];
+	}
+	
+	auto productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:names];
+	productsRequest.delegate = viewController;
+	[productsRequest start];
 }
 
 void SystemIos::purchase(const std::string& product)
 {
-    //
 }
 
 void SystemIos::haptic(HapticType hapticType)
 {
-    if (hapticType == HapticType::Low)
-    {
-        auto generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleLight];
-        [generator impactOccurred];
-    }
-    else if (hapticType == HapticType::Medium)
-    {
-        auto generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleMedium];
-        [generator impactOccurred];
-    }
-    else if (hapticType == HapticType::High)
-    {
-        auto generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleHeavy];
-        [generator impactOccurred];
-    }
+	if (hapticType == HapticType::Low)
+	{
+		auto generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleLight];
+		[generator impactOccurred];
+	}
+	else if (hapticType == HapticType::Medium)
+	{
+		auto generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleMedium];
+		[generator impactOccurred];
+	}
+	else if (hapticType == HapticType::High)
+	{
+		auto generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleHeavy];
+		[generator impactOccurred];
+	}
 }
 
 #endif
