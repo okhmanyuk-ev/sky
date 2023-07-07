15c0034a118d1d3e0767792b810b8dc5ffed12ac
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2023-07-07 18:33:53 +0300

    crash when trying to log without console

--- src/console/device.cpp
+++ src/console/device.cpp
@@ -3,16 +3,10 @@
 
 void sky::Log(const std::string& text)
 {
-	if (!ENGINE->hasSystem<Console::Device>())
-		return;
-
 	CONSOLE_DEVICE->writeLine(text);
 }
 
 void sky::Log(Console::Color color, const std::string& text)
 {
-	if (!ENGINE->hasSystem<Console::Device>())
-		return;
-
 	CONSOLE_DEVICE->writeLine(text, color);
 }
