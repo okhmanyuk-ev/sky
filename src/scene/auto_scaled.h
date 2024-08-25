d1056da252238fb26d4ec992a8bfba208ababf80
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2024-08-25 18:31:50 +0300

    scene: add AutoScaled

---
+++ src/scene/auto_scaled.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <scene/node.h>
+
+namespace Scene
+{
+	template <class T> class AutoScaled : public T
+	{
+		static_assert(std::is_base_of<Node, T>::value, "T must be derived from Node");
+	protected:
+		void leaveUpdate() override
+		{
+			T::leaveUpdate();
+			
+			if (!this->hasParent())
+				return;
+
+			auto parent_size = this->getParent()->getAbsoluteSize();
+			auto scale = parent_size / this->getSize();
+			this->setScale(glm::min(scale.x, scale.y));
+		}
+	};
+}
