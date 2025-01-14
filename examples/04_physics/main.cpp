f1d00ea2c4123c22204b8ae76276dd93ca5c508e
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2025-01-14 15:56:56 +0300

    add 04_physics example

---
+++ examples/04_physics/main.cpp
@@ -0,0 +1,144 @@
+#include <sky/sky.h>
+
+
+void sky_main()
+{
+	Shared::Application app("Physics", { Shared::Application::Flag::Scene });
+
+	auto world = std::make_shared<Shared::PhysHelpers::World>();
+	world->setAnchor(0.5f);
+	world->setPivot(0.5f);
+	world->setDebug(true);
+	app.getScene()->getRoot()->attach(world);
+
+	auto floor = std::make_shared<Shared::PhysHelpers::Entity>();
+	floor->setSize({ 768.0f, 16.0f });
+	floor->setPivot(0.5f);
+	floor->setPosition({ 0.0f, 192.0f });
+	world->attach(floor);
+
+	auto floor_rect = std::make_shared<Scene::Rectangle>();
+	floor_rect->setStretch(1.0f);
+	floor_rect->setRounding(0.5f);
+	floor->attach(floor_rect);
+
+	auto enableAutoSuicide = [](std::shared_ptr<Scene::Node> node){
+		node->runAction(Actions::Collection::ExecuteInfinite([node]{
+			if (node->getY() > 1000.0f)
+				node->runAction(Actions::Collection::Kill(node));
+		}));
+	};
+
+	static glm::vec2 BoxSize = { 24.0f, 24.0f };
+	static float BallSize = 24.0f;
+	static float y = -96.0f;
+
+	auto spawnBox = [world, enableAutoSuicide]{
+		auto box = std::make_shared<Shared::PhysHelpers::Entity>();
+		box->setType(Shared::PhysHelpers::Entity::Type::Dynamic);
+		box->setSize(BoxSize);
+		box->setPivot(0.5f);
+		box->setY(y);
+		enableAutoSuicide(box);
+		world->attach(box);
+
+		auto rect = std::make_shared<Scene::Rectangle>();
+		rect->setStretch(1.0f);
+		rect->setRounding(0.5f);
+		box->attach(rect);
+	};
+
+	auto spawnBall = [world, enableAutoSuicide]{
+		auto ball = std::make_shared<Shared::PhysHelpers::Entity>();
+		ball->setType(Shared::PhysHelpers::Entity::Type::Dynamic);
+		ball->setShape(Shared::PhysHelpers::Entity::Shape::Circle);
+		ball->setSize(BallSize);
+		ball->setPivot(0.5f);
+		ball->setY(y);
+		enableAutoSuicide(ball);
+		world->attach(ball);
+
+		auto circle = std::make_shared<Scene::Circle>();
+		circle->setStretch(1.0f);
+		ball->attach(circle);
+	};
+
+	static bool auto_spawn = true;
+	static float ChanceValue = 0.5f;
+
+	world->runAction(Actions::Collection::Pausable([]{ return auto_spawn; }, Actions::Collection::RepeatInfinite([spawnBox, spawnBall]() -> std::unique_ptr<Actions::Action> {
+		const auto delay = 0.125f;
+
+		return Actions::Collection::MakeSequence(
+			Actions::Collection::Wait(delay),
+			Actions::Collection::Execute([spawnBox, spawnBall]{
+				if (sky::chance(ChanceValue))
+					spawnBox();
+				else
+					spawnBall();
+			})
+		);
+	})));
+
+	static auto framer = Common::FrameSystem::Framer();
+
+	framer.setCallback([spawnBox, spawnBall]{
+		ImGui::Begin("Options", nullptr, ImGui::User::ImGuiWindowFlags_ControlPanel);
+		ImGui::SetWindowPos(ImGui::User::TopLeftCorner());
+
+		ImGui::Checkbox("Auto spawn", &auto_spawn);
+		ImGui::Separator();
+		ImGui::DragFloat2("Box size", (float*)&BoxSize, 1.0f, 1.0f, 256.0f);
+		ImGui::DragFloat("Ball size", &BallSize, 1.0f, 1.0f, 256.0f);
+		ImGui::SliderFloat("Ball <-> Box", &ChanceValue, 0.0f, 1.0f);
+		ImGui::Separator();
+
+		if (ImGui::Button("Spawn 1 box"))
+		{
+			spawnBox();
+		}
+
+		if (ImGui::Button("Spawn 25 boxes"))
+		{
+			for (int i = 0; i < 25; i++)
+			{
+				spawnBox();
+			}
+		}
+
+		if (ImGui::Button("Spawn 100 boxes"))
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				spawnBox();
+			}
+		}
+
+		ImGui::Separator();
+
+		if (ImGui::Button("Spawn 1 ball"))
+		{
+			spawnBall();
+		}
+
+		if (ImGui::Button("Spawn 25 ball"))
+		{
+			for (int i = 0; i < 25; i++)
+			{
+				spawnBall();
+			}
+		}
+
+		if (ImGui::Button("Spawn 100 ball"))
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				spawnBall();
+			}
+		}
+
+		ImGui::End();
+	});
+
+	app.run();
+}
