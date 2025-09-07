e2fa8062491362a0d915abab9bde2a3e3ce48e4e
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2025-09-07 21:59:18 +1000

    scheduler: add Task class with useful things

--- src/sky/scheduler.h
+++ src/sky/scheduler.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <list>
-#include <functional>
-#include <mutex>
 #include <sky/locator.h>
-#include <sky/clock.h>
 #include <sky/console.h>
+#include <sky/clock.h>
+#include <functional>
+#include <list>
 
 namespace sky
 {
@@ -20,7 +19,7 @@ namespace sky
 			Continue
 		};
 
-		using Task = std::function<Status()>;
+		class Task;
 
 	public:
 		void frame();
@@ -60,5 +59,84 @@ namespace sky
 		std::optional<sky::Duration> mTimeDeltaLimit; // this can save from animation breaks
 		uint64_t mFrameCount = 0;
 		bool mChoked = false;
+	};
+
+	class Scheduler::Task
+	{
+	public:
+		Task(std::initializer_list<Task> tasks)
+		{
+			mFunc = [tasks = std::list<Task>(std::move(tasks))]() mutable {
+				if (tasks.empty())
+					return Status::Finished;
+
+				if (tasks.front()() == Status::Continue)
+					return Status::Continue;
+
+				tasks.pop_front();
+				return tasks.empty() ? Status::Finished : Status::Continue;
+			};
+		}
+
+		template <typename Func>
+			requires std::invocable<Func> &&
+			(std::same_as<std::invoke_result_t<Func>, Status> || std::same_as<std::invoke_result_t<Func>, void>)
+		Task(Func&& func)
+		{			
+			mFunc = [func = std::forward<Func>(func)]() mutable {
+				if constexpr (std::same_as<std::invoke_result_t<Func>, Status>)
+				{
+					return func();
+				}
+				else
+				{
+					func();
+					return Status::Finished;
+				}
+			};
+		}
+
+		template <typename Func>
+			requires std::invocable<Func> && (std::convertible_to<std::invoke_result_t<Func>, std::tuple<Status, std::optional<Task>>> ||
+				std::convertible_to<std::invoke_result_t<Func>, std::optional<Task>>)
+		Task(Func&& func)
+		{
+			mFunc = [func = std::forward<Func>(func), need_invoke_func = true, status = std::optional<Status>{ std::nullopt }, task = std::optional<Task>{ std::nullopt }]() mutable {
+				if (need_invoke_func)
+				{
+					if constexpr (std::convertible_to<std::invoke_result_t<Func>, std::tuple<Status, std::optional<Task>>>)
+					{
+						std::tie(status, task) = func();
+					}
+					else
+					{
+						status = Status::Finished;
+						task = func();
+					}
+					
+					need_invoke_func = false;
+				
+					if (task.has_value())
+						return Status::Continue;
+				}
+				
+				if (task.has_value())
+				{
+					if (task.value()() == Status::Continue)
+						return Status::Continue;
+				}
+				
+				need_invoke_func = true;
+				return status.value();
+			};
+		}
+
+		auto operator()()
+		{
+			return mFunc();
+		}
+
+	private:
+		std::function<Status()> mFunc;
 	};
 }
