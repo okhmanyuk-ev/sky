fc8a105b96de3b24a405d8c86235f9cbf27721c4
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2025-09-12 12:00:11 +1000

    tasks: add WaitForSeconds func

---
+++ src/sky/task.cpp
@@ -0,0 +1,12 @@
+#include "task.h"
+#include <sky/clock.h>
+#include <sky/scheduler.h>
+
+sky::Task<> sky::Tasks::WaitForSeconds(float seconds)
+{
+	while (seconds > 0.0f)
+	{
+		seconds -= sky::ToSeconds(sky::Scheduler::Instance->getTimeDelta());
+		co_await std::suspend_always{};
+	}
+}
