724c3497c5c5e56f47c15a0b3a2169741d9c9c5a
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2025-10-30 13:34:23 +1000

    rename hud_show_tasks -> hud_show_threadpool

--- src/shared/performance_console_commands.h
+++ src/shared/performance_console_commands.h
@@ -21,7 +21,7 @@ namespace Shared
 		sky::CVar<int> mWantShowDrawcalls = sky::CVar<int>("hud_show_drawcalls", 0, "show drawcalls statistics");
 		sky::CVar<int> mWantShowBatches = sky::CVar<int>("hud_show_batches", 0, "show batches statistics");
 		sky::CVar<int> mWantShowTargets = sky::CVar<int>("hud_show_targets", 0, "show render targets statistics");
-		sky::CVar<int> mWantShowTasks = sky::CVar<int>("hud_show_tasks", 0, "show tasks count on screen");
+		sky::CVar<int> mWantShowThreadpool = sky::CVar<int>("hud_show_threadpool", 0, "show threadpool tasks on screen");
 #ifndef EMSCRIPTEN
 		sky::CVar<bool> mWantShowNetSpeed = sky::CVar<bool>("hud_show_net_speed", false);
 		sky::CVar<bool> mWantShowNetPps = sky::CVar<bool>("hud_show_net_pps", false);
