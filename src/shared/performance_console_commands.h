ee23185c33e25cd9aaae199c041f21cad2eb0eeb
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2025-10-30 17:28:32 +1000

    add hud_show_tasks cvar

--- src/shared/performance_console_commands.h
+++ src/shared/performance_console_commands.h
@@ -22,6 +22,7 @@ namespace Shared
 		sky::CVar<int> mWantShowBatches = sky::CVar<int>("hud_show_batches", 0, "show batches statistics");
 		sky::CVar<int> mWantShowTargets = sky::CVar<int>("hud_show_targets", 0, "show render targets statistics");
 		sky::CVar<int> mWantShowThreadpool = sky::CVar<int>("hud_show_threadpool", 0, "show threadpool tasks on screen");
+		sky::CVar<int> mWantShowTasks = sky::CVar<int>("hud_show_tasks", 0, "show tasks on screen");
 #ifndef EMSCRIPTEN
 		sky::CVar<bool> mWantShowNetSpeed = sky::CVar<bool>("hud_show_net_speed", false);
 		sky::CVar<bool> mWantShowNetPps = sky::CVar<bool>("hud_show_net_pps", false);
