78d7fb69fa974ba8245fb9d6ca0b526a97d69a68
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2026-01-02 15:11:10 +1000

    examples: upd labels example

--- examples/05_labels/main.cpp
+++ examples/05_labels/main.cpp
@@ -17,7 +17,7 @@ void sky_main()
 	bg->attach(column);
 
 	auto createLabel = [&](const std::wstring& text) {
-		auto label = std::make_shared<Scene::Label>();
+		auto label = std::make_shared<Shared::SceneHelpers::Outlined<Scene::Label>>();
 		label->setAnchor({ 0.5f, 0.0f });
 		label->setPivot({ 0.5f, 0.0f });
 		label->setParseColorTagsEnabled(true);
@@ -35,34 +35,32 @@ void sky_main()
 	});
 
 	createLabel(L"<b>Simple Label</b>");
-
 	createLabel(L"<color=rgba(0,255,0,96)>Semi-transparent Green (RGBA)</color>");
 	createLabel(L"<color=rgb(0,0,255)>Solid Blue (RGB)</color>");
 	createLabel(L"<color=frgba(1,0,0,1)>Fully Opaque Red (FRGBA)</color>");
 	createLabel(L"<color=frgb(1,0,1)>Magenta (FRGB)</color>");
 	createLabel(L"<color=hex(00FF00FF)>Opaque Lime Green (HEX)</color>");
-
 	createLabel(L"<color=red><b>Red Text</b> with <color=blue><i>Blue Nested</i></color> and back to red</color>");
-
 	createLabel(L"<color=yellow><u>Yellow Text</u> with <color=green><b>Green Bold</b> and <color=gray>Gray Nested</color></color> ending here</color>");
-
 	createLabel(L"<color=rgba(255,0,0,128)>Semi-Transparent Red</color> mixed with <color=hex(00FF00)>Solid Green (HEX)</color>");
-
 	createLabel(L"<color=rgb(255,165,0)>Orange Text</color> combined with <color=frgba(0,1,0,0.5)><i>Translucent Green Italic</i></color>");
-
 	createLabel(L"<color=red>Red <color=blue>Blue <color=green>Green</color> Back to Blue</color> Back to Red</color>");
-
 	createLabel(L"<gradient=red_to_blue>Gradient from Red to Blue</gradient>");
 	createLabel(L"<gradient=rainbow>Rainbow Gradient Text</gradient>");
-
 	createLabel(L"<b>Bold Text</b>, <i>Italic Text</i>, and <u>Underlined Text</u>");
-
 	createLabel(L"<color=rgba(255,0,0,192)><b><i>Semi-Bold Italic Red</i></b></color>");
 	createLabel(L"<color=hex(0000FF)><u><b>Underlined Bold Blue</b></u></color>");
-
 	createLabel(L"<color=yellow>Yellow <color=green>Green <color=blue>Blue <color=red>Red <color=purple>Purple</color> Back to Red</color> Back to Blue</color> Back to Green</color> Back to Yellow</color>");
-
 	createLabel(L"<color=red>Red <color=blue>Blue <color=green>Green <color=yellow>Yellow <color=orange>Orange <color=purple>Purple <color=black>Black</color> Back to Purple</color> Back to Orange</color> Back to Yellow</color> Back to Green</color> Back to Blue</color> Back to Red</color>");
+
+	{
+		auto label = std::make_shared<Shared::SceneHelpers::Outlined<Scene::Label>>();
+		label->setAnchor({ 0.5f, 0.0f });
+		label->setPivot({ 0.5f, 0.0f });
+		label->setAlign(sky::TextMesh::Align::Center);
+		label->setText(L"multilined text\nnon word wrap");
+		column->attach(label);
+	}
 
 	app.run();
 }
