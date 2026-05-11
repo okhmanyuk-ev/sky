329e35f251785cc5bf62c1b75c4e2d1e93bfa1d3
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2026-05-11 12:12:42 +1000

    remove unused cvar defines

--- src/common/console_commands.h
+++ src/common/console_commands.h
@@ -5,28 +5,12 @@
 
 #define CVAR_GETTER(V) [this] { return std::vector<std::string>({ V }); }
 #define CVAR_GETTER2(V1, V2) [this] { return std::vector<std::string>({ V1, V2 }); }
-#define CVAR_GETTER3(V1, V2, V3) [this] { return std::vector<std::string>({ V1, V2, V3 }); }
 
 #define CVAR_GETTER_INT(V) CVAR_GETTER(std::to_string(V))
 #define CVAR_GETTER_INT2(V1, V2) CVAR_GETTER2(std::to_string(V1), std::to_string(V2))
-#define CVAR_GETTER_INT3(V1, V2, V3) CVAR_GETTER3(std::to_string(V1), std::to_string(V2), std::to_string(V3))
-#define CVAR_GETTER_INT_FUNC(V) CVAR_GETTER_INT(V())
 #define CVAR_GETTER_INT2_FUNC(V1, V2) CVAR_GETTER_INT2(V1(), V2())
 
 #define CVAR_GETTER_BOOL(V) CVAR_GETTER(std::to_string(V))
-#define CVAR_GETTER_BOOL2(V1, V2) CVAR_GETTER2(std::to_string(V1), std::to_string(V2))
-#define CVAR_GETTER_BOOL3(V1, V2, V3) CVAR_GETTER3(std::to_string(V1), std::to_string(V2), std::to_string(V3))
-#define CVAR_GETTER_BOOL_FUNC(V) CVAR_GETTER_BOOL(V())
-
-#define CVAR_GETTER_FLOAT(V) CVAR_GETTER(std::to_string(V))
-#define CVAR_GETTER_FLOAT2(V1, V2) CVAR_GETTER2(std::to_string(V1), std::to_string(V2))
-#define CVAR_GETTER_FLOAT3(V1, V2, V3) CVAR_GETTER3(std::to_string(V1), std::to_string(V2), std::to_string(V3))
-#define CVAR_GETTER_FLOAT_FUNC(V) CVAR_GETTER_FLOAT(V())
-
-#define CVAR_GETTER_DOUBLE(V) CVAR_GETTER(std::to_string(V))
-#define CVAR_GETTER_DOUBLE2(V1, V2) CVAR_GETTER2(std::to_string(V1), std::to_string(V2))
-#define CVAR_GETTER_DOUBLE3(V1, V2, V3) CVAR_GETTER3(std::to_string(V1), std::to_string(V2), std::to_string(V3))
-#define CVAR_GETTER_DOUBLE_FUNC(V) CVAR_GETTER_DOUBLE(V())
 
 #define CON_ARGS_NAME args
 #define CON_ARGS const std::vector<std::string>& CON_ARGS_NAME
@@ -35,21 +19,12 @@
 #define CON_ARG_INT(N) stoi(CON_ARG(N))
 #define CON_ARG_BOOL(N) static_cast<bool>(stoi(CON_ARG(N)))
 #define CON_ARG_FLOAT(N) stof(CON_ARG(N))
-#define CON_ARG_DOUBLE(N) stod(CON_ARG(N))
 
 #define CVAR_SETTER(V) [this](CON_ARGS) { V; }
 
-#define CVAR_SETTER_FLOAT(V) CVAR_SETTER(V = CON_ARG_FLOAT(0))
-#define CVAR_SETTER_FLOAT_FUNC(V) CVAR_SETTER(V(CON_ARG_FLOAT(0)))
-
-#define CVAR_SETTER_DOUBLE(V) CVAR_SETTER(V = CON_ARG_DOUBLE(0))
-#define CVAR_SETTER_DOUBLE_FUNC(V) CVAR_SETTER(V(CON_ARG_DOUBLE(0)))
-
 #define CVAR_SETTER_BOOL(V) CVAR_SETTER(V = CON_ARG_BOOL(0))
-#define CVAR_SETTER_BOOL_FUNC(V) CVAR_SETTER(V(CON_ARG_BOOL(0)))
 
 #define CVAR_SETTER_INT(V) CVAR_SETTER(V = CON_ARG_INT(0))
-#define CVAR_SETTER_INT_FUNC(V) CVAR_SETTER(V(CON_ARG_INT(0)))
 #define CVAR_SETTER_INT2_FUNC(V) CVAR_SETTER(V(CON_ARG_INT(0), CON_ARG_INT(1)))
 
 #define CMD_METHOD(V) [this](CON_ARGS) { V(CON_ARGS_NAME); }
